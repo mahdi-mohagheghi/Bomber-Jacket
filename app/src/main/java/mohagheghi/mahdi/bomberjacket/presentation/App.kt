6468ea9cbbeef26472571093f994aacf86d6150c
Andrew <itsmahdi2000@gmail.com>
2024-12-31 09:55:27 +0100

    feat(app): init koin
    init and start koin in App class

--- app/src/main/java/mohagheghi/mahdi/bomberjacket/presentation/App.kt
+++ app/src/main/java/mohagheghi/mahdi/bomberjacket/presentation/App.kt
@@ -1,6 +1,25 @@
 package mohagheghi.mahdi.bomberjacket.presentation
 
 import android.app.Application
+import mohagheghi.mahdi.bomberjacket.data.di.localModule
+import org.koin.android.ext.koin.androidContext
+import org.koin.android.ext.koin.androidLogger
+import org.koin.core.context.GlobalContext.startKoin
+import org.koin.core.logger.Level
 
 class App : Application() {
+
+    override fun onCreate() {
+        super.onCreate()
+
+        initKoin()
+    }
+
+    private fun initKoin() {
+        startKoin {
+            androidLogger(level = Level.ERROR)
+            androidContext(this@App)
+            modules(localModule)
+        }
+    }
 }
